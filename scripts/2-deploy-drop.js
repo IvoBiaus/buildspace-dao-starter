0186845142e3b32154d7db2c100be3d443b4edb0
Ivo Biaus <ivobiaus@gmail.com>
2021-12-30 20:28:31 -0300

    Deploy NFT bundle script.

---
+++ scripts/2-deploy-drop.js
@@ -0,0 +1,28 @@
+import { ethers } from "ethers";
+import { readFileSync } from "fs";
+
+import sdk from "./1-initialize-sdk.js";
+
+const app = sdk.getAppModule("0x2234A6Ed9B7A1cb1f83782b1A035F34ceEF2945E");
+
+(async () => {
+  try {
+    const bundleDropModule = await app.deployBundleDropModule({
+      name: "LastnameDAO Membership",
+      description: "A DAO where your lastname is represented on an shared NFT.",
+      image: readFileSync("scripts/assets/lastname.png"),
+      primarySaleRecipientAddress: ethers.constants.AddressZero,
+    });
+
+    console.log(
+      "✅ Successfully deployed bundleDrop module, address:",
+      bundleDropModule.address
+    );
+    console.log(
+      "✅ bundleDrop metadata:",
+      await bundleDropModule.getMetadata()
+    );
+  } catch (error) {
+    console.log("failed to deploy bundleDrop module", error);
+  }
+})();
--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ThirdwebWeb3Provider } from "@3rdweb/hooks";
+
 import "./index.css";
 import App from "./App.jsx";
 
-// Render the App component to the DOM
+// Chains you wanna support.
+// 4 = Rinkeby.
+const supportedChainIds = [4];
+
+// Type of wallet you want to support.
+// In this case, we support Metamask which is an "injected wallet".
+const connectors = {
+  injected: {},
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThirdwebWeb3Provider
+      connectors={connectors}
+      supportedChainIds={supportedChainIds}
+    >
+      <App />
+    </ThirdwebWeb3Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
